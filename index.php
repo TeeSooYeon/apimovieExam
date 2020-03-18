2d54c001c9dd5c8c8fed6e4442dd79dad6328bed
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-03-18 13:31:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ if($login_button == true){
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	  	<link rel="stylesheet" type="text/css" href="css/util.css">
 		<link rel="stylesheet" type="text/css" href="css/main.css">
+	    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body style="display: inline-block;">
 	  	<div style="margin: 0 auto;">
