f076aa8c65d133a7ee21389c869204bb030ee815
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-03-19 19:03:40 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -29,22 +29,24 @@ if(isset($_REQUEST['submit'])){
 		<title> Movie - Admin Login </title>
 	</head>
 	<body>
+		<div class="limiter">
 		<div class="container-login100" style="background-image: url("images/bg-01.jpg");">
 			<div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
 			<form class="login100-form validate-form flex-sb flex-w" method="POST" action="" name="login">
 				<br><br>
 				<?php if($message == null){}else{echo "<center>".$message."</center>";} ?>
-				<div>
-					<input class="wrap-input100 validate-input" type="text" required name="adminid" placeholder="Username" autocomplete="off"/>
+				<div class="wrap-input100 validate-input">
+					<input class="input100" type="text" required name="adminid" placeholder="Username" autocomplete="off"/>
 				</div>
-				<div>
-					<input class="wrap-input100 validate-input" type="password" required name="password" placeholder="Password"/>
+				<div class="wrap-input100 validate-input" >
+					<input class="input100" type="password" required name="password" placeholder="Password"/>
 				</div>
-				<div>
+				<div class="container-login100-form-btn m-t-17">
 					<input class="login100-form-btn" type="submit" name="submit" value="Login"/>
 				</div>
 			</form>
 			</div>
+		</div>
 		</div>
 	</body>
 </html>
